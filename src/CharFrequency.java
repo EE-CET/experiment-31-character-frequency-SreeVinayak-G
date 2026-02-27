abf85e862e300cf8a57fc05ca8617a6d20313835
SREEVINAYAKG <srivinayakg2005@gmail.com>
2026-02-27 20:47:49 +0530

    Submitted charFreq

--- src/CharFrequency.java
+++ src/CharFrequency.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 public class CharFrequency {
-    
-        // TODO: Read the string S (Use sc.nextLine() to capture spaces)
-        // TODO: Read the character C
-        // TODO: Iterate through the string and count occurrences of C
-        // TODO: Print the count
-
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        char c = sc.next().charAt(0);
+        int cnt=0;
+        for(int i=0;i<s.length();i++){
+                if(s.charAt(i)==c){
+                        cnt++;
+                }
+        }
+        System.out.println(cnt);
+    }
 }
